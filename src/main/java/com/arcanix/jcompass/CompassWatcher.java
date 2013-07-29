faf80640549d45c7b217a2b20286f3745dd26f96
Jean-Philippe Ricard <ricardjp@arcanix.com>
2013-07-28 21:40:37 -0400

    refactored watcher

--- src/main/java/com/arcanix/jcompass/CompassWatcher.java
+++ src/main/java/com/arcanix/jcompass/CompassWatcher.java
@@ -5,6 +5,8 @@ import org.apache.commons.vfs2.FileSystemException;
 import org.apache.commons.vfs2.FileSystemManager;
 import org.apache.commons.vfs2.VFS;
 import org.apache.commons.vfs2.impl.DefaultFileMonitor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 
@@ -12,6 +14,8 @@ import java.io.File;
  * @author ricardjp@arcanix.com (Jean-Philippe Ricard)
  */
 public final class CompassWatcher {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CompassWatcher.class);
 
     private final CompassCompiler compassCompiler;
 
@@ -23,6 +27,8 @@ public final class CompassWatcher {
     }
 
     public void watch() throws FileSystemException {
+
+        LOGGER.info("Lauching compass watcher...");
         FileSystemManager manager = VFS.getManager();
         FileObject file = manager.resolveFile(
                 this.compassCompiler.getConfigFile().getParentFile().getAbsolutePath());
